d0f13a1223014c2adabd115d45e15b00e0f13e03
Cassandra Ortiz <Cassy.Ortiz5@gmail.com>
2019-12-19 10:14:44 -0800

    Updates to Javascript and added pages

--- js/over21.js
+++ js/over21.js
@@ -1,21 +1,46 @@
 
-
+'use strict'
 
 var today = new Date();
 var years = today.getFullYear();
 var hourNow = today.getHours();
-var agereq  = prompt('How old are you?');
+
+var code = confirm('Are you ready to code?');
+var msg;
+
+
+if (code){
+    msg = "Let's get drunk and write some code!";
+}
+else{
+    msg = "Don't want to code? Your loss!";
+}
+
+document.write('<h1>' + msg + '</h1>');
 
 var greeting;
+var under;
 
-if (agereq >= 21) {20
+var agereq  = prompt('How old are you?');
+
+if (agereq >= 21) {
     greeting = 'Enjoy your drink!';
+    under = false;
 } else if (agereq > 18) {
-    greeting = 'You are not old enough, but do not tell your parents';
+    greeting = 'Do your parents know you are trying to drink?';
+    under = true;
 } else if (agereq > 0) {
     greeting = 'Please do not drink - you are too young.';
+    under = true;
 } else {
     greeting = 'Please be responsible.';
 }
 
-document.write('<h1>' + greeting + '</h1>');
+alert(greeting);
+
+if (under) {
+    window.location.href = "http://127.0.0.1:5500/projects/mybiz/under21.html";;
+}
+
+
+
--- js/testing.js
+++ js/testing.js
@@ -1,16 +1,2 @@
 
-var today = new Date();
-var years = today.getFullYear();
-var User = prompt("What is your birthday?");
-
-Var findyear = User.getFullYear();
-
-Var AgeReq;
-
-if (years-findyear >=21) {
-    AgeReq = "Good to go!"
-} else {
-    AgeReq = "Not old enough!"
-}
-
-document.write("<h1>"+agereq+"</h1>");
+var 
